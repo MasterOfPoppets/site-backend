6e3eac9fbb42c785640dd322a732d31556baf33a
Gareth Hughes <gurrkin@gmail.com>
2016-09-10 14:47:16 +0100

    Adding some actually useful data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 import { apolloExpress, graphiqlExpress } from 'apollo-server';
 import { makeExecutableSchema } from 'graphql-tools';
 
 import Schema from './schema/schema.graphql';
+import Col from './schema/col.graphql';
+
+import colData from '../src/data/cols.json';
 
 const app = express();
 
@@ -16,16 +20,29 @@ const resolvers = {
     aNumber() {
       return 1;
     },
+    aTest() {
+      return 2;
+    },
+    cols() {
+      return colData;
+    },
   },
+  Mutation: {
+    greet(_, { name }) {
+      return `Hello there, ${name}!`;
+    }
+  }
 };
 
 const jsSchema = makeExecutableSchema({
-  typeDefs: [Schema],
+  typeDefs: [Schema, Col],
   resolvers,
   logger,
   allowUndefinedInResolve: false, //optional
   resolverValidationOptions: {}, //optional
 });
+
+app.use('*', cors());
 
 app.use('/graphql', bodyParser.json(), apolloExpress({
   schema: jsSchema
